4457c698e4347a8633b685da1e17a5abdef430f6
gr1tc0d3r <gr1tc0d3r@gmail.com>
2022-05-31 12:58:33 -0700

    Square Every Digit - 7Kyu

---
+++ challenge053122-4.js
@@ -0,0 +1,13 @@
+// Square Every Digit
+
+// P - Take in a number
+// R - A new number that is each digit of the number squared
+// E - 9119 => 811181
+
+function squareDigits(num) {
+    num.toString();
+    num.split(',');
+    num = finalNum.forEach(Number(Math.pow(num, 2)))
+    num = finalNum.join('');
+    return num;
+}
