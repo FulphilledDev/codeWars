d30309761a38ba5493d6f41e60c26d16d04b3c89
Philip Simpson <dev.phits@gmail.com>
2022-06-16 07:24:20 -0700

    Keep Hydrated - 8kyu

---
+++ challenge061622-1.js
@@ -0,0 +1,11 @@
+// 8 kyu - Keep Hydrated
+
+// P - time (hours)
+// R - litres needed to consume
+// E - 2 --> 1; 0.82 --> 0; 11.8 --> 5
+
+//Create function litres that takes in the parameter of hours
+function litres(hours) {
+    //Equation to solve for litres that rounds
+    return Math.floor(hours / 2);
+}
