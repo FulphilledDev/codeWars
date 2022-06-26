d578c9971a63a3119a83fbfa07cb137a925efbcc
Philip Simpson <dev.phits@gmail.com>
2022-06-26 06:07:12 -0700

    8kyu - Parse nice int from char problem

---
+++ challenge062622.js
@@ -0,0 +1,7 @@
+// 8kyu - Parse Nice int from char problem
+
+// P - inputString ('x years old')
+// R - Number(x)
+// E - "4 years old" --> 4
+
+const getAge = (inputString) => Number(inputString[0])
