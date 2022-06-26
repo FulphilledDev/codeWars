053b0b0b0593b93ff45cb87b08ea19ccf9a984e8
Philip Simpson <dev.phits@gmail.com>
2022-06-25 18:58:13 -0700

    Convert a String to a Number - 8kyu

---
+++ challenge062522-1.js
@@ -0,0 +1,10 @@
+// 8kyu - Convert a String to a Number
+
+// P - String
+// R - Number
+// E - "1234" --> 1234
+
+const stringToNumber = function (str) {
+    // Evaluate value within string
+    return eval(str)
+}
