f171d51182ad90daab3ad2969dbcbd8a12c89b7a
Philip Simpson <dev.phits@gmail.com>
2022-06-27 06:16:59 -0700

    Switch It Up!

---
+++ challenge062722.js
@@ -0,0 +1,32 @@
+// 8kyu - Switch it Up!
+
+// P - number 0-9
+// R - string word
+// E - 1 --> "one"
+
+function switchItUp (number) {
+    switch (number) {
+        case 0 : 
+            return 'Zero'
+        case 1 : 
+            return 'One'
+        case 2 : 
+            return 'Two'
+        case 3 : 
+            return 'Three'
+        case 4 : 
+            return 'Four'
+        case 5 : 
+            return 'Five'
+        case 6 : 
+            return 'Six'
+        case 7 : 
+            return 'Seven'
+        case 8 : 
+            return 'Eight'
+        case 9 : 
+            return 'Nine'
+        default:
+            break;
+    }
+}
