4652fd0f00259f433e70309536555ad2dc5d6f04
Philip Simpson <dev.phits@gmail.com>
2022-06-30 06:38:03 -0700

    8kyu - Check for Factor

---
+++ challenge063022.js
@@ -0,0 +1,7 @@
+// 8kyu - Grasshopper: Check for factor
+
+// P - base, factor
+// R - true if factor is factor of base : false
+// E - (2450, 5) --> true
+
+const checkForFactor = (base, factor) => base % factor == 0 ? true : false
