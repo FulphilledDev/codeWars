88169e06f38ec0a0a9e71fe6414bf3f8ce60bd24
Philip Simpson <dev.phits@gmail.com>
2022-07-03 07:12:31 -0700

    8kyu - Do I Get a Bonus?

--- challenge070322.js
+++ challenge070322.js
@@ -4,4 +4,7 @@
 // R - if bonus === true return salary * 10, else salary
 // E - 25000, true ==> £250000
 
-const bonusTime = (salary, bonus) => bonus === true ? `$${salary*10}` : `$${salary}`
+const bonusTime = (salary, bonus) => {
+    
+    bonus === true ? `£${salary*10}` : `£${salary}`
+}
