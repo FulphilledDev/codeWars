700935317c56d7e9821aa525aaa8d547dd9d5824
Philip Simpson <dev.phits@gmail.com>
2022-07-05 16:18:07 -0700

    8kyu - Template Strings

---
+++ challenge070522-2.js
@@ -0,0 +1,9 @@
+// 8kyu - Template Strings
+
+// P - obj, feature
+// R - string using template string feature
+// E - ('Animals', 'Good') ==> 'Animals are Good'
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+}
