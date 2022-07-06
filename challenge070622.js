5d5a6b47d2d29192240116e65bd95854a13e4f37
Philip Simpson <dev.phits@gmail.com>
2022-07-06 16:35:27 -0700

    8kyu - Count Sheep

--- challenge070622.js
+++ challenge070622.js
@@ -4,7 +4,6 @@
 // R - counted sheep
 // E - countSheep(2) ==> '1 sheep...2 sheep...'
 
-const countSheep = num => {
-    let countedSheep =`${num} sheep...`
-    return countedSheep.padEnd(num, `${num} sheep...`)
+var countSheep = function (length) {
+    return Array.from({ length }, (x, y) => ++y + ' sheep...').join('')
 }
