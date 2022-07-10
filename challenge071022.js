ef501fcacc4ae48ad2dd39cb9d37762acaf400d8
Philip Simpson <dev.phits@gmail.com>
2022-07-10 07:25:53 -0700

    Debugging: Get Planet Name by ID

---
+++ challenge071022.js
@@ -0,0 +1,39 @@
+// 8kyu - Debugging: Get Planet Name by ID
+
+// Add break between each case
+// Add default break at end
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+      break;
+    default: 
+      `Please put in an id number between 1 and 8`
+      break;
+  }
+  
+  return name;
+}
