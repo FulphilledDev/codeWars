7b1a88b43fa472a3f8f480effa2b43dd372f0297
Philip Simpson <dev.phits@gmail.com>
2022-07-13 15:39:35 -0700

    8kyu - Return Strings

---
+++ challenge071322-2.js
@@ -0,0 +1,9 @@
+// 8kyu - Returning Strings
+
+// P - Name
+// R - Greeting with name
+// E - Philip --> 'Hello, Philip how are you doing today?'
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
