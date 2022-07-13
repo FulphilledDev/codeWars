e81e58ea5c236d6a7bc88c41fffed88d2529c77f
Philip Simpson <dev.phits@gmail.com>
2022-07-13 15:46:29 -0700

    8kyu - Beginner-Lost Without a Map

---
+++ challenge071322-3.js
@@ -0,0 +1,10 @@
+// 8kyu - Beginner-Lost Without a Map
+
+// P - Array of integers
+// R - Array of doubled integers
+// E - [1, 2, 3] --> [2, 4, 6]
+
+function maps(x) {
+    // Map a new array while multiplying each value by 2
+    return x.map(x => x * 2)
+}
