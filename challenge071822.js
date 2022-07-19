9fe9eac4a7d40a8297d7a2cb32f4481d34982bfb
Philip Simpson <dev.phits@gmail.com>
2022-07-18 17:11:54 -0700

    8kyu - Invert Values

---
+++ challenge071822.js
@@ -0,0 +1,8 @@
+// 8kyu - Invert Values
+
+// P - Array
+// R - Inverse Array
+// E - invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+
+// Create new array with inverse of each 'x' value
+const invert = array => array.map(x => -x)
