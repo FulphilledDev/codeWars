349f21db2827efe56e900277d89e775fa2ff791f
Philip Simpson <dev.phits@gmail.com>
2022-07-28 13:10:48 -0700

    8kyu - Is he going to survive?

---
+++ challenge072822-1.js
@@ -0,0 +1,10 @@
+// 8kyu - Is he going to survive?
+
+// P - number of bullets and dragons
+// R - true or false
+// E - hero(10, 5) --> true; hero(7, 4) --> false
+
+// Is number of bullets greater than or equal to twice the number of dragons
+const hero = (bullets, dragons) => bullets >= (dragons * 2) ? true : false;
+
+
