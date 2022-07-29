67bdc9600444880ea206eb7d39f0719997c74424
Philip Simpson <dev.phits@gmail.com>
2022-07-29 16:51:44 -0700

    8kyu - Volume of a Cuboid

---
+++ challenge072922.js
@@ -0,0 +1,11 @@
+// 8kyu - Volume of a Cuboid
+
+// P - length, width, height
+// R - product
+// E - (6.3, 2, 5) ==> 63
+
+class Kata {
+  static getVolumeOfCuboid(l, w, h) {
+    return l*w*h
+  }
+}
