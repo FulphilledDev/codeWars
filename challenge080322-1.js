1e09016326df664fd5c953fb3d3a70222e6a135a
Philip Simpson <dev.phits@gmail.com>
2022-08-03 11:59:27 -0700

    8kyu - Find Max and Min Values of a List

---
+++ challenge080322-1.js
@@ -0,0 +1,8 @@
+// 8kyu - Find Max and Min Values of a List
+
+// P - List of integers
+// R - Max and Min number in list
+// E - [4,6,2,1,9,63,-134,566] --> max = 566, min = -134
+
+const max = list => Math.max(...list)
+const min = list => Math.min(...list)
