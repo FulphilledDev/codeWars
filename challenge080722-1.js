834425c8393abc3a65d6586b608606840ca489c9
Philip Simpson <dev.phits@gmail.com>
2022-08-07 17:11:14 -0700

    8kyu - Convert a string to an array

---
+++ challenge080722-1.js
@@ -0,0 +1,8 @@
+// 8kyu - Convert a string to an array
+
+// P - string
+// R - array of string items
+// E - ("Robin Singh") --> ["Robin", "Singh"]
+
+// split string into an array of substrings with pattern as parameter
+const stringToArray = string => string.split(' ')
