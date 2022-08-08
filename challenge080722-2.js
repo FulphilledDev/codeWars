2c1712f87bfc26c215c7bc1e1a666af8f77f0d50
Philip Simpson <dev.phits@gmail.com>
2022-08-07 17:19:25 -0700

    7kyu - Sum of odd numbers

---
+++ challenge080722-2.js
@@ -0,0 +1,8 @@
+// 7kyu - Sum of odd numbers
+
+// P - number of row of odd numbers
+// R - sum of number row
+// E - (42) --> 74088
+
+// Sum of each 'row' is a perfect cube
+const rowSumOddNumbers = n => n ** 3
