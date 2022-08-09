f11e52854d612dcceadb2df05f28513938a957b2
Philip Simpson <dev.phits@gmail.com>
2022-08-09 10:03:25 -0700

    8kyu - Reversed Words

---
+++ challenge081022-2.js
@@ -0,0 +1,7 @@
+// 8kyu - Reversed Words
+
+// P - string
+// R - reversed order of string
+// E - ("hello world!") --> "world! hello"
+
+const reverseWords = str => str.split(' ').reverse().join(' ')
