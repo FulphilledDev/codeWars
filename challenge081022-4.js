73e9188dbc3ca4061e3837c05ea8a768baf40541
Philip Simpson <dev.phits@gmail.com>
2022-08-10 17:37:50 -0700

    8kyu - Printing Array elements with comma delimiters

---
+++ challenge081022-4.js
@@ -0,0 +1,11 @@
+// 8kyu - Printing Array elements with Comma delimiters
+
+// P - Array of elements
+// R - String with comma defined elements of array
+// E - ["h","o","l","a"] --> "h,o,l,a"
+
+const printArray = array => array.toString()
+
+// OR
+
+// const printArray2 = arr => arr.join()
