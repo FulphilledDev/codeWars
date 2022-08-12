1e1f53421a6d911fec126b278b1fceabea1d07aa
Philip Simpson <dev.phits@gmail.com>
2022-08-12 11:17:32 -0700

    7kyu - Sort Arrays-1 (Lexicographical)

---
+++ challenge081222-4.js
@@ -0,0 +1,9 @@
+// 7kyu - Sort arrays-1
+
+// P - array of elements
+// R - sorted in lexicographical order
+// E - ['one', 'two', 'three' ] --> ["one", "three", "two"]
+
+sortme = function (names) {
+    return names.sort()
+}
