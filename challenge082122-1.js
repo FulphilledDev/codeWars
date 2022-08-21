4181f199f804172e7c5c9267bb78f0d79990b9ae
Philip Simpson <dev.phits@gmail.com>
2022-08-21 13:55:11 -0700

    7kyu - List to Array

---
+++ challenge082122-1.js
@@ -0,0 +1,14 @@
+// 7kyu - List to Array
+
+// P - list of objects
+// R - array of objects
+// E - {value: 1, next: {value: 2, next: {value: 3, next: null}}} --> [1, 2, 3]
+
+function listToArray(list) {
+    // Create empty array to push items to
+    let arr = [];
+    // Push each item in the list to empty array
+    for (let item = list; item; item = item.next)
+        arr.push(item.value);
+    return arr;
+}
