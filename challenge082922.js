236fafd6e7f38f505120017dc03da6083c62000b
Philip Simpson <dev.phits@gmail.com>
2022-08-29 15:55:52 -0700

    8kyu - Set Alarm

---
+++ challenge082922.js
@@ -0,0 +1,7 @@
+// 8kyu - L1: Set Alarm
+
+// P - (booleans) --> employed, vacation
+// R - true if employed === true and vacation === false
+// E - (true, true) --> false
+
+const setAlarm = (e, v) => e && !v;
