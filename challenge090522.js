09a7da37fe249766f8bb39e66a9264598b93e7ff
Philip Simpson <dev.phits@gmail.com>
2022-09-05 15:41:45 -0700

    8kyu - Remove Exclamation Marks

---
+++ challenge090522.js
@@ -0,0 +1,9 @@
+// 8kyu - Remove Exclamation Marks
+
+// P - string
+// R - without exclamation marks
+// E - "Hello World!" --> "Hello World"
+
+function removeExclamationMarks (s) {
+  return s.replace(/\!/g, '')
+  }
