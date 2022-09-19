e07a308e1e799349db1e480b0cdfb212dc895fa2
Philip Simpson <dev.phits@gmail.com>
2022-09-18 20:27:51 -0700

    8kyu - Sum the Strings

---
+++ challenge0922-1.js
@@ -0,0 +1,10 @@
+// 8kyu - Sum the Strings
+
+// P - 2 string integers
+// R - sum as a string
+// E - "4", "5" --> "9"
+
+const sumStr = (a, b) => String(+a + +b)
+
+// Also Written as...
+// const sumStr = (a, b) => String(Number(a) + Number(b))
