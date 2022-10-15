81c7c364885e00da63745ba2bddae22a817cd2dc
Philip Simpson <dev.phits@gmail.com>
2022-10-15 15:55:01 -0700

    8kyu - Stringy Strings

---
+++ challenge101522.js
@@ -0,0 +1,9 @@
+// 8kyu - Stringy Strings
+
+// P - Number (size)
+// R - String of alternating 1s and 0s equal to size
+// E - size === 6 => '101010'
+
+// Syntax for padStart( targetLength, padString )
+
+const stringy = size => ''.padStart(size, '10')
